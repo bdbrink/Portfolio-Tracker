d0273d7a75c4c4f28a659beae0f87a49d9d861c9
bdbrink <brendan.brink@outlook.com>
2023-06-06 17:54:50 -0500

    fixing func nameing and general cleanup of test

--- stockQuote_test.go
+++ stockQuote_test.go
@@ -1,38 +1,29 @@
+// stockQuote_test.go
+
 package main
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
-func TestGetStockQuote(t *testing.T) {
-	// Create a mock HTTP server
-	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Check the request URL
-		if r.URL.Path != "/stock/aapl/quote" {
-			t.Errorf("Expected request URL '/stock/aapl/quote', got '%s'", r.URL.Path)
-		}
+func TestCurrentMarketData(t *testing.T) {
+	symbol, price, name := currentMarketData("AAPL")
 
-		// Send a mock response
-		response := `{"symbol":"AAPL","price":135.25}`
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(response))
-	}))
-	defer mockServer.Close()
-
-	// Replace the original base URL with the mock server URL
-	baseURL = mockServer.URL
-
-	// Perform the test
-	quote, err := getStockQuote("AAPL")
-	if err != nil {
-		t.Errorf("Error while getting stock quote: %s", err)
+	// Verify the results
+	expectedSymbol := "AAPL"
+	if symbol != expectedSymbol {
+		t.Errorf("Expected symbol '%s', got '%s'", expectedSymbol, symbol)
 	}
 
 	// Verify the results
-	expected := StockQuote{Symbol: "AAPL", Price: 135.25}
-	if quote != expected {
-		t.Errorf("Expected quote %+v, got %+v", expected, quote)
+	expectedPrice := 0.0 // You can update this with the expected price value
+	if price != expectedPrice {
+		t.Errorf("Expected price '%f', got '%f'", expectedPrice, price)
+	}
+
+	// Verify the results
+	expectedName := "" // You can update this with the expected name value
+	if name != expectedName {
+		t.Errorf("Expected name '%s', got '%s'", expectedName, name)
 	}
 }
